7c27029ca4b9cbc3efa6ac53035c458f13ba86e2
Crista Lopes <lopes@uci.edu>
2023-11-30 18:48:25 +0000

    Make the script read the csv file. Reduced the csv file by a lot.

--- Commit-Message-Generation-Tutorial/commit_message_tutorial_script.py
+++ Commit-Message-Generation-Tutorial/commit_message_tutorial_script.py
@@ -11,6 +11,7 @@ Original file is located at
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import csv
 
 load_dotenv()
 
@@ -18,52 +19,52 @@ load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 client = OpenAI(api_key=api_key)
 
-# Start an infinite loop to continuously interact with the user
-while True:
-    # Prompt the user for input
-    user_input = input("Enter your message (or type 'exit' to quit): ")
+prompt = "Generate a commit message for this diff:\n"
+# Read the input file
+with open('Commit_Messages.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    n = 2
+    for row in reader:
+        prompt += row["Code Change"] 
 
-    # Check if the user wants to terminate the program
-    if user_input.lower() == 'exit':
-        break
+        # Send the user input to OpenAI and get a response
+        response = client.chat.completions.create(
+            # Specifies the model to be used for the response
+            model="gpt-4-1106-preview",
 
-    # Send the user input to OpenAI and get a response
-    response = client.chat.completions.create(
-      # Specifies the model to be used for the response
-      model="gpt-4-1106-preview",
+            # The list of messages to be processed by the model
+            messages=[
+            {
+                "role": "user",  # Defines the role of the message sender (user or assistant)
+                "content": prompt  # The actual message content
+            }
+            ],
 
-      # The list of messages to be processed by the model
-      messages=[
-        {
-          "role": "user",  # Defines the role of the message sender (user or assistant)
-          "content": user_input  # The actual message content
-        }
-      ],
+            # Controls the randomness in the response generation
+            # Higher values mean more creative responses
+            temperature=1,
 
-      # Controls the randomness in the response generation
-      # Higher values mean more creative responses
-      temperature=1,
+            # The maximum number of tokens to generate in the response
+            max_tokens=256,
 
-      # The maximum number of tokens to generate in the response
-      max_tokens=256,
+            # Controls diversity via nucleus sampling: 0.5 means half of all likelihood-weighted options are considered
+            top_p=1,
 
-      # Controls diversity via nucleus sampling: 0.5 means half of all likelihood-weighted options are considered
-      top_p=1,
+            # Adjusts the likelihood of repeating the same line verbatim
+            frequency_penalty=0,
 
-      # Adjusts the likelihood of repeating the same line verbatim
-      frequency_penalty=0,
+            # Penalizes new tokens based on their existing frequency in the text so far
+            presence_penalty=0
+        )
 
-      # Penalizes new tokens based on their existing frequency in the text so far
-      presence_penalty=0
-    )
-
-    # Process and display the response from OpenAI
-    if response.choices:
-        assistant_message = response.choices[0].message.content
-        print(assistant_message)
-    else:
-        # Handle cases where no response is received
-        print("No response from the assistant.")
-
+        # Process and display the response from OpenAI
+        if response.choices:
+            assistant_message = response.choices[0].message.content
+            print(f'Row {n}:\n {assistant_message}\n')
+        else:
+            # Handle cases where no response is received
+            print("No response from the assistant.")
+        n += 1
+        
 # Print a message when the program is terminated
 print("Program exited.")
